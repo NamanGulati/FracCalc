dc146138e43e2a5f950c54a3aca2061c62305c0b
Naman Gulati <gula1214@gmail.com>
2015-04-14 20:52:33 -0700

    Update FracCalc.java

--- FracCalc.java
+++ FracCalc.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import javax.swing.*;
  
-public class FracCalcMultiOp {
+public class FracCalc {
     public static void main(String[] args) {
  
         Scanner scan = new Scanner(System.in);
